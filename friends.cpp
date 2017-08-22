abd7b441a606444c8ec02bc6a44436b3bdc56d04
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2017-08-22 13:15:56 -0300

    Create friends.cpp

---
+++ friends.cpp
@@ -0,0 +1,32 @@
+// // //
+// Trabalho 1 de TAG 
+// 150032552 - Claudio Segala Rodrigues Silva Filho
+// 000000000 - Luiz Gebrim
+// // //
+
+#include <vector.h>
+#include <string.h>
+#include <iostream.h>
+#include <stdlib.h>
+
+using namespace std;
+
+void clear_screen() {
+  system("clear || cls");
+} 
+  
+int main() {
+  clear_screen();
+  
+  enum options {
+    findClick,
+    printGraph,
+    leave
+  };
+  
+  do {
+    cout << "Welcome" << endl;
+  } while(op == );
+  
+  
+}
